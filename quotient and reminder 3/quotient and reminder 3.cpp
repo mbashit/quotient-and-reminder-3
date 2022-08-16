1bb3c395f6d04388badf8e712963506d49e30a6d
mbashit <michaelbashitapwa52@gmail.com>
2022-08-16 22:12:37 +0300

    Add project files.

---
+++ quotient and reminder 3/quotient and reminder 3.cpp
@@ -0,0 +1,21 @@
+// quotient and reminder 3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int number, N;
+
+	cout << "enter the integer number:";
+	cin >> number;
+
+	N = number % 2;
+
+	cout << "the result is :" << N << endl;
+	return 0;
+
+		
+}
+
